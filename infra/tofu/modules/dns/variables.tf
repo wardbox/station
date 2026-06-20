48a912cccbc7bf95000d77461029fbef83f3ec01
wardbox <dylank@startmail.com>
2026-06-19 23:55:00 -0700

    chore: validate dns module tailnet ip

--- infra/tofu/modules/dns/variables.tf
+++ infra/tofu/modules/dns/variables.tf
@@ -8,4 +8,9 @@ variable "argo_tailnet_ip" {
   description = "Tailscale IP for argo.<zone>."
   type        = string
   nullable    = false
+
+  validation {
+    condition     = can(cidrhost("${var.argo_tailnet_ip}/32", 0)) && cidrcontains("100.64.0.0/10", var.argo_tailnet_ip)
+    error_message = "argo_tailnet_ip must be a valid Tailscale IPv4 address in 100.64.0.0/10."
+  }
 }
